53727ffd7faf11f14b0c1eeb8ecec153b8939af3
Yeon Ju <joyeonjulife@gmail.com>
2026-03-20 10:10:12 +0900

    counter HTML onclick

---
+++ 02_counter/main.js
@@ -0,0 +1,20 @@
+let number = 0;
+// h1에 있는 숫자 요소를 가져오자
+const resultH1 = document.getElementById("div")[0];
+// button에 있는 + 요소를 가져오자
+const plusButton = document.getElementsByClassName("botton")[0];
+
+// function plus(){
+//     number++;
+//     resultH1.innterHTML = number;
+// }
+
+// plusButton.onclick = () =>{
+//     number++;
+//     resultH1.innterHTML = number;
+// }
+
+const plus = () =>{
+    number++;
+    resultH1.innterHTML = number;
+}
