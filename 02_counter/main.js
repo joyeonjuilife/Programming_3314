0eefe79e694ca9acc6b22feae33824e61960973f
Yeon Ju <joyeonjulife@gmail.com>
2026-03-27 08:58:10 +0900

    counter HTML 0 삭제,JS number만 남기자, render() from ytm0181

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,6 +1,6 @@
 let number = 0;
 // h1에 있는 숫자 요소를 가져오자
-const resultH1 = document.getElementById("div")[0];
+const resultH1 = document.getElementById("num");
 // button에 있는 + 요소를 가져오자
 const plusButton = document.getElementsByClassName("botton")[0];
 
@@ -14,11 +14,23 @@ const plusButton = document.getElementsByClassName("botton")[0];
 //     resultH1.innterHTML = number;
 // }
 
+function render(){
+    resultH1.innerHTML = number;
+}
+
+
 const plus = (value = 1) =>{
     number += value;
-    resultH1.innterHTML = number;
+    render();
 }
+const minus = (value = 0) =>{
+    number--;
+    render();
+}
+
 const reset = () =>{
     number = 0;
-    resultH1.innterHTML = number;
+    render();
 }
+
+render();
